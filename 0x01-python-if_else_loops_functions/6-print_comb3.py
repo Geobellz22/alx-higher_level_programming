96a8e51a6424f9624bbd25d59eb10a56046c524f
BelloSaheed <st377126@gmail.com>
2022-12-12 04:32:01 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for k in range((i+1), 10):
-        if i != 7 or k != 8:
+        if i != 8 or k != 9:
             print("{}{}," .format(i, k))
         else:
             print("{}{}" .format(i, k), end='')
