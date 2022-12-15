7de5dcf10f5ffa71066023c0f3d122f17c97adb5
BelloSaheed <st377126@gmail.com>
2022-12-15 04:13:15 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if size - 1 == 0:
         print("{} arguments.".format(size - 1))
     elif size - 1 == 1:
-        print("{} arguments:".format(size - 1))
+        print("{} argument:".format(size - 1))
         print("{}: {}".format(size - 1, sys.argv[1]))
     else:
         print("{} arguments:".format(size - 1))
