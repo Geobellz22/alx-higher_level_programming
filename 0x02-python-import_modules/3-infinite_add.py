0b625880149a5c4c065b5857f1629d3ddfaa6557
BelloSaheed <st377126@gmail.com>
2022-12-16 01:01:54 -0800

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     add = 0
-    for idx in rang(len(sys.argv)):
+    for idx in sys.argv
         if idx != sys.argv[0]:
             add += int(idx)
         print(add)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 if__name__ == "__main__":
-    import sys
+   from file variable_load_5 import a
+    print("{}".format(a))
