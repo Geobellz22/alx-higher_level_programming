0538dcf4a0033df64f8eaf2a76ee735b179d11ff
BelloSaheed <st377126@gmail.com>
2022-12-16 01:07:45 -0800

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     add = 0
     for idx in sys.argv
-        if idx != sys.argv[0]:
-            add += int(idx)
-        print(add)
+    if idx != sys.argv[0]:
+        add += int(idx)
+    print(add)
