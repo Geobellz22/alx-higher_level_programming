64815cb91b2f7d3357e8219a02d8cfca12a2b37d
BelloSaheed <st377126@gmail.com>
2022-12-28 06:57:56 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for chr in my_string:
-        if chr == 'c' and chr == 'C':
+    for ele in my_string:
+        if ele == 'c' and ele == 'C':
             continue
         else:
-            new_string += chr
+            new_string += ele
     return new_string
