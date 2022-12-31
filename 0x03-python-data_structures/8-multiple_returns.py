9ee3fdc053cd0144fe6f2d7d3c2e71059052d49c
BelloSaheed <st377126@gmail.com>
2022-12-31 14:52:27 -0800

    Task 7

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == ():
-        return None
+    if len(sentence) == 0:
+        return 0, None
     else:
-        return len(sentence)
+        return len(sentence), sentence[0]
