91e6564001d84ba35b7c88a23c21dfb10ff0bb41
BelloSaheed <st377126@gmail.com>
2023-01-04 08:36:37 -0800

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for idx in range(x):
+            if var(my_list[idx]) is int:
+                print("{:d}".format(my_list[idx], end='')
+               count  = count + 1
+            else:
+                continue
+    except ValueError:
+        pass
+        print()
+        return count
+
+            
