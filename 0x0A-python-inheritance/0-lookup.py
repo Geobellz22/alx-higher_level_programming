746be76c012a082d16e5f9256e2df7a634b78f76
BelloSaheed <st377126@gmail.com>
2023-01-10 05:40:50 -0800

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup
+contains lookup method that returns list of objects attributes and methods
+"""
+
+
+def lookup(obj):
+    """returns list of objects's attributes and methods
+    """
+    return dir(obj)
