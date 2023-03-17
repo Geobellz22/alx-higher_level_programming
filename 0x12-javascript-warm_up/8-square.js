5c4a59dd175abe877ce3fafe8be982419dd5171c
BelloSaheed <st377126@gmail.com>
2023-03-17 10:32:44 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const x = Number(process.argv[2]);
   for (let i = 0; i < x; i++) {
-  console.log('X'.repeat(x));
-}
+    console.log('X'.repeat(x));
+  }
 }
