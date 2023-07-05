51e01b8f1266aa14592fc2e71cdfbc0c9efaf3da
Geobelz22 <st377126@gmail.com>
2023-07-05 15:59:04 -0700

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL
+and an email sends a post request
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as res:
+        body = response.read().decode('utf-8')
+    print(body)
