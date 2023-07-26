95bd57ba7289d4fca457ca9891444dab917d3d1b
Geobellz22 <st377126@gmail.com>
2023-07-26 22:59:43 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const args = process.argv
+const args = process.argv;
 
 fs.readFile(`${args[2]}`, 'utf8', (err, data) => {
   if (err) {
-	console.error(err);
-	return;
+    console.error(err);
+    return;
   }
 
   // Print the content of the file
-	console.log(data);
+  console.log(data);
 });
