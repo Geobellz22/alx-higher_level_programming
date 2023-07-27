1381192c80e39d818d6e62cc6be0833a37ecaf5f
Geobellz22 <st377126@gmail.com>
2023-07-27 11:07:56 +0000

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ const args = process.argv;
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url + `${args[2]}`, (error, response, body) => {
   if (error) {
-	console.log(error);
+    console.log(error);
   }
   console.log(JSON.parse(body).title);
 });
